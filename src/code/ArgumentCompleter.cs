9e74f74573b05055d3d117a40c4a6182022ed2f3
Anam Navied <anam.naviyou@gmail.com>
2021-07-19 10:57:31 -0400

    attempt to add argument completer for installed package names

--- src/code/ArgumentCompleter.cs
+++ src/code/ArgumentCompleter.cs
@@ -1,12 +1,14 @@
-﻿using System.ComponentModel;
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
 using Microsoft.PowerShell.PowerShellGet.UtilClasses;
+using Microsoft.PowerShell.PowerShellGet.Cmdlets;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Management.Automation;
 using System.Management.Automation.Language;
+using NuGet.Versioning;
 
 internal class RepositoryNameCompleter : IArgumentCompleter
 {
@@ -33,4 +35,31 @@ internal class RepositoryNameCompleter : IArgumentCompleter
             }
         }
     }
+}
+
+internal class InstalledPackagesNameCompleter : IArgumentCompleter
+{
+    public IEnumerable<CompletionResult> CompleteArgument(
+        string commandName,
+        string parameterName,
+        string wordToComplete,
+        CommandAst commandAst,
+        IDictionary fakeBoundParameters)
+        {
+            wordToComplete = Utils.TrimQuotes(wordToComplete);
+            wordToComplete = String.IsNullOrWhiteSpace(wordToComplete) ? "*" : wordToComplete + "*";
+            // var wordToCompletePattern = WildcardPattern.Get(
+            //     pattern: String.IsNullOrWhiteSpace(wordToComplete) ? "*" : wordToComplete + "*",
+            //     options: WildcardOptions.IgnoreCase);
+
+
+            GetHelper getHelper = new GetHelper(null); // null won't work for Verbose/DEbug/errors I think...
+            foreach (PSResourceInfo pkg in getHelper.FilterPkgPaths(
+                name: new string[] {wordToComplete},
+                versionRange: VersionRange.All, 
+                pathsToSearch: Utils.GetAllResourcePaths(null))) // null probably won't work!
+            {
+                yield return new CompletionResult(Utils.QuoteName(pkg.Name));
+            }
+        }
 }
